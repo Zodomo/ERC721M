896ec481aba396d0de674c13c3270da23778031a
Zodomo <zodomo@proton.me>
2023-07-30 17:03:47 -0500

    reworked withdraw to work if renounced

--- src/ERC721M.sol
+++ src/ERC721M.sol
@@ -121,7 +121,13 @@ contract ERC721M is Ownable, AlignedNFT {
         address _to,
         uint256 _tokenId
     ) public virtual onlyOwner { vault.rescueERC721(_address, _to, _tokenId); }
-    function withdrawFunds(address _to, uint256 _amount) public virtual onlyOwner { _withdrawFunds(_to, _amount); }
+    function withdrawFunds(address _to, uint256 _amount) public virtual {
+        // If renounced, send to fundsRecipient only
+        if (owner() == address(0)) { _to = fundsRecipient; }
+        // Otherwise, apply ownership check
+        else if (owner() != msg.sender) { revert Unauthorized(); }
+        _withdrawFunds(_to, _amount);
+    }
 
     // Internal handling for receive() and fallback() to reduce code length
     function _processPayment() internal {
