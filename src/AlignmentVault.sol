bc28eec4bea940df9bc613be6583837408756967
Zodomo <zodomo@proton.me>
2023-09-26 17:00:48 -0500

    remove console2

--- src/AlignmentVault.sol
+++ src/AlignmentVault.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.20;
 
-import "forge-std/console2.sol";
 import "openzeppelin/interfaces/IERC20.sol";
 import "openzeppelin/interfaces/IERC721.sol";
 import "openzeppelin/proxy/utils/Initializable.sol";
