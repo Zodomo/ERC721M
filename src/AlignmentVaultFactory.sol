02beeee00cb8c9d0322df810172d8a704fcbf28c
Zodomo <zodomo@proton.me>
2023-09-26 23:07:30 -0500

    begin alignmentvault composability refactor

---
+++ src/AlignmentVaultFactory.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.20;
+
+import "./AlignmentVault.sol";
+import "solady/utils/LibClone.sol";
+
+interface IInitialize {
+    function initialize(address _erc721, uint256 _vaultId) external;
+    function disableInitializers() external;
+}
+
+// This is a WIP contract
+// Author: Zodomo // Zodomo.eth // X: @0xZodomo // T: @zodomo // zodomo@proton.me
+// https://github.com/Zodomo/ERC721M
+contract AlignmentVaultFactory is Ownable {
+
+    event Deployed(address indexed deployer, address indexed collection);
+
+    address public implementation;
+
+    constructor(address _owner, address _implementation) payable {
+        _initializeOwner(_owner);
+        implementation = _implementation;
+    }
+
+    // Deploy MiyaMints flavored ERC721M collection
+    function deploy(address _erc721, uint256 _vaultId) public returns (address deployment) {
+        deployment = LibClone.clone(implementation);
+        emit Deployed(msg.sender, deployment);
+
+        IInitialize(deployment).initialize(_erc721, _vaultId);
+        IInitialize(deployment).disableInitializers();
+    }
+
+    function deployDeterministic(
+        address _erc721,
+        uint256 _vaultId,
+        bytes32 _salt
+    ) public returns (address deployment) {
+        deployment = LibClone.cloneDeterministic(implementation, _salt);
+        emit Deployed(msg.sender, deployment);
+
+        IInitialize(deployment).initialize(_erc721, _vaultId);
+        IInitialize(deployment).disableInitializers();
+    }
+}
